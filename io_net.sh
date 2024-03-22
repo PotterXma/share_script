31adfac58f637f51aeb95df91142de0f55793f26
PotterXma <125950917+PotterXma@users.noreply.github.com>
2024-03-22 14:26:08 +0800

    Update io_net.sh

--- io_net.sh
+++ io_net.sh
@@ -72,8 +72,8 @@ config_json=$(cat <<EOF
 EOF
 )
 
-# 将配置文件写入 /etc/v2ray/config.json
-echo "$config_json" | sudo tee /etc/v2ray/config.json > /dev/null
+# 将配置文件写入 /etc/v2raya/config.json
+echo "$config_json" | sudo tee /etc/v2raya/config.json > /dev/null
 
 # 启动 v2ray 服务
 sudo systemctl enable v2raya.service --now
